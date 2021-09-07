7c5d74c3d4bfc28794ef12a76b081917851a2845
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2021-09-07 20:21:45 +0300

    add notifications support

--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
@@ -1,7 +1,13 @@
 package org.hyperskill.stopwatch
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -12,10 +18,13 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import org.hyperskill.stopwatch.StringUtil.time
 import java.util.concurrent.atomic.AtomicInteger
 
 const val ONE_SECOND: Long = 1000
+const val CHANNEL_ID = "org.hyperskill"
 
 val COLORS = arrayOf(Color.RED, Color.GREEN, Color.BLUE)
 
@@ -27,8 +36,26 @@ class MainActivity : AppCompatActivity() {
     private var tickTimer: Runnable? = null
     private var upperLimit = AtomicInteger(0)
 
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        createNotificationChannel()
         setContentView(R.layout.activity_main)
 
 
@@ -50,8 +77,8 @@ class MainActivity : AppCompatActivity() {
                             Integer.parseInt(editText.text.toString())
                         } catch (e: NumberFormatException) {
                             0
-                        }
-                        )                    }
+                        })
+                    }
                     .setNegativeButton(android.R.string.cancel) { dialog, _ ->
                         dialog.dismiss()
                     }
@@ -72,6 +99,9 @@ class MainActivity : AppCompatActivity() {
                 Color.BLACK
             }
 
+            if (textColor == Color.RED) {
+                this.notifyOutOfTime()
+            }
             handler.post {
                 textView.text = getString(R.string.counter, time(minutes), time(seconds))
                 textView.setTextColor(textColor)
@@ -107,6 +137,25 @@ class MainActivity : AppCompatActivity() {
                 setTime(0)
             }
         }
+    }
+
+    private fun notifyOutOfTime() {
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Get up")
+                .setContentText("Time exceeded")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+        NotificationManagerCompat.from(this).notify(393939, builder.build())
     }
 
     override fun onStop() {
